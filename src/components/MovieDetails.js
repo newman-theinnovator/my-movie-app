37b8831631c65061af90eae3feb812f2c9aea372
Newman <newman.ukpakatheboss@gmail.com>
2023-09-11 11:13:42 +0100

    time to style

--- src/components/MovieDetails.js
+++ src/components/MovieDetails.js
@@ -1,10 +1,39 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
+import axios from '../apiConfig'; // Import your Axios configuration
 
 function MovieDetails() {
+  const { id } = useParams(); // Get the movie ID from the route parameter
+  const [movieDetails, setMovieDetails] = useState(null); // State to store movie details
+
+  useEffect(() => {
+    // Function to fetch movie details by ID
+    const fetchMovieDetails = async () => {
+      try {
+        const response = await axios.get(`/movie/${id}`);
+        setMovieDetails(response.data);
+      } catch (error) {
+        console.error('Error fetching movie details:', error);
+      }
+    };
+
+    // Call the fetch function when the component mounts
+    fetchMovieDetails();
+  }, [id]);
+
   return (
     <div>
       <h2>Movie Details</h2>
-      {/* Add content for movie details */}
+      {movieDetails ? (
+        <div>
+          <h3>{movieDetails.title}</h3>
+          <p>Release Date: {movieDetails.release_date}</p>
+          <p>Runtime: {movieDetails.runtime} minutes</p>
+          <p>{movieDetails.overview}</p>
+        </div>
+      ) : (
+        <p>Loading...</p>
+      )}
     </div>
   );
 }
